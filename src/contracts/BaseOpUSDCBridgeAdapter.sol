0788c140bf1792a81bd5c7a4e4138967cb1dff8f
excaliborr <excalibor@defi.sucks>
2024-05-07 13:12:36 -0400

    fix: lint

--- src/contracts/BaseOpUSDCBridgeAdapter.sol
+++ src/contracts/BaseOpUSDCBridgeAdapter.sol
@@ -2,8 +2,8 @@
 pragma solidity 0.8.25;
 
 import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
-import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import {IOpUSDCBridgeAdapter} from 'interfaces/IOpUSDCBridgeAdapter.sol';
 
 abstract contract BaseOpUSDCBridgeAdapter is Ownable, IOpUSDCBridgeAdapter {
