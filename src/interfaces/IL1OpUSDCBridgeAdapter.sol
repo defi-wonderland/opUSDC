07361f6073df4c520b6014b8d35e8336f10fcf9c
Hex <165055168+hexshire@users.noreply.github.com>
2024-05-20 18:18:21 -0300

    fix: missing return value
    Co-authored-by: Disco <131301107+0xDiscotech@users.noreply.github.com>

--- src/interfaces/IL1OpUSDCBridgeAdapter.sol
+++ src/interfaces/IL1OpUSDCBridgeAdapter.sol
@@ -49,11 +49,10 @@ interface IL1OpUSDCBridgeAdapter {
   //////////////////////////////////////////////////////////////*/
 
   /**
-   * @notice Address of the Upgrade Manager contract
-   * @return _address The address of the Upgrade Manager contract
+   * @return _upgradeManager The address of the Upgrade Manager contract
    */
   // solhint-disable-next-line func-name-mixedcase
-  function UPGRADE_MANAGER() external view returns (address);
+  function UPGRADE_MANAGER() external view returns (address _upgradeManager);
 
   /**
    * @notice Fetches the amount of USDC tokens that will be burned when the burnLockedUSDC function is called
