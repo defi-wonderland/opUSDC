9672f544552717e5456f7b923afd08f22ea12a9a
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2024-06-24 18:55:04 -0300

    chore: remove unused initialize function

--- src/contracts/utils/USDCInitTxs.sol
+++ src/contracts/utils/USDCInitTxs.sol
@@ -2,25 +2,10 @@
 pragma solidity 0.8.25;
 
 /**
- * @notice Library containing the initialization transactions constants for the USDC implementation contract defined
- * by Circle.
+ * @notice Library containing the initialization transactions constants (without the first one) for the USDC
+ *  implementation contract defined by Circle.
  */
 library USDCInitTxs {
-  /**
-   * @dev The `initialize()` transaction data for the USDC implementation contract.
-   */
-  bytes public constant INITIALIZE = abi.encodeWithSignature(
-    'initialize(string,string,string,uint8,address,address,address,address)',
-    '',
-    '',
-    '',
-    0,
-    address(1),
-    address(1),
-    address(1),
-    address(1)
-  );
-
   /**
    * @dev The `initializeV2()` transaction data for the USDC implementation contract.
    */
