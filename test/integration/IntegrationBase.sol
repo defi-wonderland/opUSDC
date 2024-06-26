a30491740743434a4fa67f2ffe8b0d1fa81e80ee
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2024-06-26 16:39:54 -0300

    refactor: set a very low gas limit on l1 integration

--- test/integration/IntegrationBase.sol
+++ test/integration/IntegrationBase.sol
@@ -37,7 +37,7 @@ contract IntegrationBase is Helpers {
   uint32 public constant MIN_GAS_LIMIT_DEPLOY = 8_000_000;
   uint32 internal constant _ZERO_VALUE = 0;
   uint256 internal constant _amount = 1e18;
-  uint32 internal constant _minGasLimit = 1_000_000;
+  uint32 internal constant _minGasLimit = 1000;
 
   /// @notice Value used for the L2 sender storage slot in both the OptimismPortal and the
   ///         CrossDomainMessenger contracts before an actual sender is set. This value is
