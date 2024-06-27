cf8b7202727a86ab6eda0e37803b2d29a09d52a7
drgorillamd <83670532+drgorillamd@users.noreply.github.com>
2024-06-27 16:01:46 +0200

    chore: refactor

--- test/invariants/fuzz/OpUSDC.t.sol
+++ test/invariants/fuzz/OpUSDC.t.sol
@@ -1,39 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 
-import {EchidnaTest} from '../AdvancedTestsUtils.sol';
-
-// https://github.com/crytic/building-secure-contracts/blob/master/program-analysis/echidna/advanced/testing-bytecode.mdq
-import {USDC_IMPLEMENTATION_CREATION_CODE} from 'script/utils/USDCImplementationCreationCode.sol';
-
-import {L1OpUSDCBridgeAdapter} from 'contracts/L1OpUSDCBridgeAdapter.sol';
-import {IL1OpUSDCFactory, L1OpUSDCFactory} from 'contracts/L1OpUSDCFactory.sol';
-import {L2OpUSDCBridgeAdapter} from 'contracts/L2OpUSDCBridgeAdapter.sol';
-import {L2OpUSDCFactory} from 'contracts/L2OpUSDCFactory.sol';
-import {USDCInitTxs} from 'contracts/utils/USDCInitTxs.sol';
-import {IL2OpUSDCFactory} from 'interfaces/IL2OpUSDCFactory.sol';
-
+import {SetupOpUSDC} from './SetupOpUSDC.sol';
 import {IL1OpUSDCBridgeAdapter} from 'interfaces/IL1OpUSDCBridgeAdapter.sol';
-import {IOpUSDCBridgeAdapter} from 'interfaces/IOpUSDCBridgeAdapter.sol';
-import {IUSDC} from 'interfaces/external/IUSDC.sol';
-
-import {ITestCrossDomainMessenger} from 'test/utils/interfaces/ITestCrossDomainMessenger.sol';
 
 //solhint-disable custom-errors
-contract OpUsdcTest is EchidnaTest {
-  IUSDC usdcMainnet;
-  IUSDC usdcBridged;
-
-  L1OpUSDCBridgeAdapter public l1Adapter;
-  L1OpUSDCFactory public factory;
-
-  L2OpUSDCBridgeAdapter public l2Adapter;
-  L2OpUSDCFactory public l2Factory;
-
-  MockBridge public mockMessenger;
-  Create2Deployer public create2Deployer;
-  address internal _usdcMinter = address(bytes20(uint160(uint256(keccak256('usdc.minter')))));
-
+contract OpUsdcTest is SetupOpUSDC {
   /////////////////////////////////////////////////////////////////////
   //                         Ghost variables                         //
   /////////////////////////////////////////////////////////////////////
@@ -42,11 +14,9 @@ contract OpUsdcTest is EchidnaTest {
   uint256 internal _L1CurrentUserNonce;
   address internal _xdomSenderDuringCall; // Who called a previous function (amongst the agents)
 
-  constructor() {
-    IL1OpUSDCFactory.L2Deployments memory _l2Deployments = _mainnetSetup();
-    _l2Setup(_l2Deployments);
-    _setupUsdc();
-  }
+  /////////////////////////////////////////////////////////////////////
+  //                           Properties                            //
+  /////////////////////////////////////////////////////////////////////
 
   // debug: echidna debug setup
   function fuzz_testDeployments() public {
@@ -108,7 +78,6 @@ contract OpUsdcTest is EchidnaTest {
   // New messages should not be sent if the state is not active 1
   // User who bridges tokens should receive them on the destination chain 2
   // Amount locked on L1 == amount minted on L2 3
-
   function fuzz_noMessageIfNotActiveL2(address _to, uint256 _amount, uint32 _minGasLimit) public AgentOrDeployer {
     // Insure we're using the correct xdom sender
     require(mockMessenger.xDomainMessageSender() == address(l2Adapter));
@@ -177,83 +146,6 @@ contract OpUsdcTest is EchidnaTest {
     } else {
       assert(_L1PreviousUserNonce == _L1CurrentUserNonce - 1);
     }
-  }
-
-  /////////////////////////////////////////////////////////////////////
-  //                     Echidna context fuzzer                      //
-  /////////////////////////////////////////////////////////////////////
-
-  /////////////////////////////////////////////////////////////////////
-  //                          Initial setup                          //
-  /////////////////////////////////////////////////////////////////////
-
-  function _setupUsdc() internal {
-    hevm.prank(usdcMainnet.masterMinter());
-    usdcMainnet.configureMinter(address(_usdcMinter), type(uint256).max);
-  }
-
-  // Deploy: USDC L1, factory L1, L1 adapter
-  function _mainnetSetup() internal returns (IL1OpUSDCFactory.L2Deployments memory _l2Deployments) {
-    address targetAddress;
-    uint256 size = USDC_IMPLEMENTATION_CREATION_CODE.length;
-    bytes memory _usdcBytecode = USDC_IMPLEMENTATION_CREATION_CODE;
-
-    assembly {
-      targetAddress := create(0, add(_usdcBytecode, 0x20), size) // Skip the 32 bytes encoded length.
-    }
-
-    usdcMainnet = IUSDC(targetAddress);
-
-    bytes[] memory usdcInitTxns = new bytes[](3);
-    usdcInitTxns[0] = USDCInitTxs.INITIALIZEV2;
-    usdcInitTxns[1] = USDCInitTxs.INITIALIZEV2_1;
-    usdcInitTxns[2] = USDCInitTxs.INITIALIZEV2_2;
-
-    factory = new L1OpUSDCFactory(address(usdcMainnet));
-
-    mockMessenger = MockBridge(0x4200000000000000000000000000000000000007);
-
-    // owner is this contract, as managed in the agents handler
-    _l2Deployments =
-      IL1OpUSDCFactory.L2Deployments(address(this), USDC_IMPLEMENTATION_CREATION_CODE, usdcInitTxns, 3_000_000);
-
-    (address _l1Adapter, address _l2Factory, address _l2Adapter) =
-      factory.deploy(address(mockMessenger), address(this), _l2Deployments);
-
-    l2Factory = L2OpUSDCFactory(_l2Factory);
-    l1Adapter = L1OpUSDCBridgeAdapter(_l1Adapter);
-    l2Adapter = L2OpUSDCBridgeAdapter(_l2Adapter);
-  }
-
-  // Send a (mock) message to the L2 messenger to deploy the L2 factory and the L2 adapter (which deploys usdc L2 too)
-  function _l2Setup(IL1OpUSDCFactory.L2Deployments memory _l2Deployments) internal {
-    IL2OpUSDCFactory.USDCInitializeData memory usdcInitializeData = IL2OpUSDCFactory.USDCInitializeData(
-      factory.USDC_NAME(), factory.USDC_SYMBOL(), usdcMainnet.currency(), usdcMainnet.decimals()
-    );
-
-    bytes memory _l2factoryConstructorArgs = abi.encode(
-      address(l1Adapter),
-      _l2Deployments.l2AdapterOwner,
-      _l2Deployments.usdcImplementationInitCode,
-      usdcInitializeData, // encode?
-      _l2Deployments.usdcInitTxs // encodePacked?
-    );
-
-    bytes memory _l2FactoryInitCode = bytes.concat(type(L2OpUSDCFactory).creationCode, _l2factoryConstructorArgs);
-
-    // !!!! Nonce incremented to avoid collision !!!
-    mockMessenger.relayMessage(
-      mockMessenger.messageNonce() + 1,
-      address(factory),
-      address(0x13b0D85CcB8bf860b6b79AF3029fCA081AE9beF2),
-      0,
-      3_000_000,
-      abi.encodeWithSignature(
-        'deploy(uint256,bytes32,bytes)', 0, factory.deploymentsSaltCounter() + 1, _l2FactoryInitCode
-      )
-    );
-
-    usdcBridged = IUSDC(l2Adapter.USDC());
   }
 
   /////////////////////////////////////////////////////////////////////
@@ -332,7 +224,7 @@ contract OpUsdcTest is EchidnaTest {
     }
   }
 
-  // Send a call to the L1 or L2 adapter (simulating a direct interaction with the bridge, from an invalid xdom sender)
+  // Send a call to the L1 or L2 adapter (simulating a direct interaction with the bridge to send the crosschain msg)
   function generateMessageToL1(
     uint256 _selectorIndex,
     address _addressA,
@@ -407,64 +299,4 @@ contract OpUsdcTest is EchidnaTest {
   }
 
   function generateCallFactory() public AgentOrDeployer {}
-}
-
-/////////////////////////////////////////////////////////////////////
-//                             L2 mock                             //
-/////////////////////////////////////////////////////////////////////
-
-// Relay any message
-contract MockBridge is ITestCrossDomainMessenger {
-  uint256 public messageNonce;
-  address public l1Adapter;
-
-  address internal _currentXDomSender;
-
-  function OTHER_MESSENGER() external view returns (address) {
-    return address(0);
-  }
-
-  function xDomainMessageSender() external view returns (address) {
-    return _currentXDomSender;
-  }
-
-  function sendMessage(address _target, bytes calldata _message, uint32 _minGasLimit) external {
-    _currentXDomSender = msg.sender;
-    messageNonce++;
-    _target.call(_message);
-  }
-
-  function relayMessage(
-    uint256 _nonce,
-    address _sender,
-    address _target,
-    uint256 _value,
-    uint256 _minGasLimit,
-    bytes calldata _message
-  ) external payable {
-    _currentXDomSender = msg.sender;
-    messageNonce++;
-    (bool succ, bytes memory ret) = _target.call{value: _value}(_message);
-
-    if (!succ) {
-      revert(string(ret));
-    }
-  }
-}
-
-// Identical to the OZ implementation used
-contract Create2Deployer {
-  // solhint-disable custom-errors
-  function deploy(uint256 _value, bytes32 _salt, bytes memory _initCode) public returns (address) {
-    address addr;
-    require(address(this).balance >= _value, 'Create2: insufficient balance');
-    require(_initCode.length != 0, 'Create2: bytecode length is zero');
-
-    assembly {
-      addr := create2(_value, add(_initCode, 0x20), mload(_initCode), _salt)
-    }
-    require(addr != address(0), 'Create2: Failed on deploy');
-
-    return addr;
-  }
 }
