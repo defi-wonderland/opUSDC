e087662f690d5f96326653b281c5c5bb14f4dfcb
excaliborr <excalibor@defi.sucks>
2024-07-03 10:10:12 -0400

    fix: revert script

--- script/sepolia/DeployBase.s.sol
+++ script/sepolia/DeployBase.s.sol
@@ -9,7 +9,7 @@ import {USDCInitTxs} from 'src/contracts/utils/USDCInitTxs.sol';
 
 contract DeployBase is Script {
   address public constant L1_MESSENGER = 0xC34855F4De64F1840e5686e64278da901e261f20;
-  uint32 public constant MIN_GAS_LIMIT_DEPLOY = 1_500_000;
+  uint32 public constant MIN_GAS_LIMIT_DEPLOY = 8_000_000;
   IL1OpUSDCFactory public immutable L1_FACTORY = IL1OpUSDCFactory(vm.envAddress('L1_FACTORY_SEPOLIA'));
 
   address public deployer = vm.rememberKey(vm.envUint('SEPOLIA_DEPLOYER_PK'));
