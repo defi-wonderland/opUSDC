990a88d1b3436ab6dc8f0e564a7f5daeb4c1acfe
Disco <131301107+0xDiscotech@users.noreply.github.com>
2024-07-08 14:11:55 -0300

    feat: add deploy on base mainnet script (#95)
    * fix: min gas limit on mainnet scripts
    
    * chore: remove unused vars on scripts

--- script/sepolia/L1FactoryDeployAndSetup.s.sol
+++ script/sepolia/L1FactoryDeployAndSetup.s.sol
@@ -8,8 +8,6 @@ import {IL1OpUSDCFactory} from 'interfaces/IL1OpUSDCFactory.sol';
 
 contract L1FactoryDeployAndSetup is Script {
   address public constant USDC = 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238;
-  address public constant L1_MESSENGER_OP = 0x58Cc85b8D04EA49cC6DBd3CbFFd00B4B8D6cb3ef;
-  address public constant L1_MESSENGER_BASE = 0xC34855F4De64F1840e5686e64278da901e261f20;
   address public deployer = vm.rememberKey(vm.envUint('SEPOLIA_DEPLOYER_PK'));
 
   function run() public {
@@ -18,6 +16,7 @@ contract L1FactoryDeployAndSetup is Script {
     console.log('Deploying L1OpUSDCFactory ...');
     IL1OpUSDCFactory _l1Factory = new L1OpUSDCFactory(USDC);
     console.log('L1OpUSDCFactory deployed at:', address(_l1Factory));
+    /// NOTE: Hardcode the address on `L1_FACTORY_SEPOLIA` inside the `.env` file
 
     vm.stopBroadcast();
   }
