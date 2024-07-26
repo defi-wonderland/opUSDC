e3a076342adf62a87d47573e547b1a8e6f33d3e8
Hex <165055168+hexshire@users.noreply.github.com>
2024-07-26 15:44:02 -0300

    Update src/contracts/L2OpUSDCBridgeAdapter.sol
    Co-authored-by: excaliborr <124819095+excaliborr@users.noreply.github.com>

--- src/contracts/L2OpUSDCBridgeAdapter.sol
+++ src/contracts/L2OpUSDCBridgeAdapter.sol
@@ -199,7 +199,7 @@ contract L2OpUSDCBridgeAdapter is IL2OpUSDCBridgeAdapter, OpUSDCBridgeAdapter {
   /**
    * @notice Receive the message from the other chain and mint the bridged representation for the user
    * @dev This function should only be called when receiving a message to mint the bridged representation
-   * @dev If the mint fails the funds might be recovered by calling withdrawBlacklistedFunds
+   * @dev If the mint fails the funds might be recovered by calling withdrawBlacklistedFunds if the user is ever unblacklisted
    * @param _user The user to mint the bridged representation for
    * @param _spender The address that provided the tokens
    * @param _amount The amount of tokens to mint
