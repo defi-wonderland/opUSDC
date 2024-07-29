e0d5fdd4cbbf632c045fef38af92024d0cbe52d8
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2024-07-29 20:16:21 -0300

    chore: unify linked adapter modifier

--- src/contracts/L2OpUSDCBridgeAdapter.sol
+++ src/contracts/L2OpUSDCBridgeAdapter.sol
@@ -42,16 +42,6 @@ contract L2OpUSDCBridgeAdapter is IL2OpUSDCBridgeAdapter, OpUSDCBridgeAdapter {
   uint256[50] private __gap;
 
   /**
-   * @notice Modifier to check if the sender is the linked adapter through the messenger
-   */
-  modifier onlyLinkedAdapter() {
-    if (msg.sender != MESSENGER || ICrossDomainMessenger(MESSENGER).xDomainMessageSender() != LINKED_ADAPTER) {
-      revert IOpUSDCBridgeAdapter_InvalidSender();
-    }
-    _;
-  }
-
-  /**
    * @notice Construct the OpUSDCBridgeAdapter contract
    * @param _usdc The address of the USDC Contract to be used by the adapter
    * @param _messenger The address of the messenger contract
