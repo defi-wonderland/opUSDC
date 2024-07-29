04297cf9c89d1f95ae6e50aa003cdc7bfcd3a0e4
excaliborr <excalibor@defi.sucks>
2024-07-29 08:56:04 -0400

    fix: lint

--- src/contracts/L1OpUSDCBridgeAdapter.sol
+++ src/contracts/L1OpUSDCBridgeAdapter.sol
@@ -265,9 +265,8 @@ contract L1OpUSDCBridgeAdapter is IL1OpUSDCBridgeAdapter, OpUSDCBridgeAdapter {
   }
 
   /**
-   * @notice Withdraws the blacklisted funds from L2 once the adapter is deprecated
-   * @dev If the _spender is still blacklisted, the chain operator will be forced to
-   *      replay this message
+   * @notice Receives a message from L2 if the adapter is deprecated are a user is withdrawing blacklisted funds
+   * @dev If the _spender is still blacklisted, the chain operator will be forced to replay this message
    * @param _spender The user that initialy provided the tokens
    * @param _amount The amount of tokens to withdraw
    */
