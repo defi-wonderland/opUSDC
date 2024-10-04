a21706de120f6db40cac0bf40344cb27dca941b4
0xDiscotech <131301107+0xDiscotech@users.noreply.github.com>
2024-10-04 16:36:57 -0300

    chore: let usdc mainnet hardcoded on l1 factory deploy

--- script/mainnet/deploy/L1FactoryDeployAndSetup.s.sol
+++ script/mainnet/deploy/L1FactoryDeployAndSetup.s.sol
@@ -7,14 +7,14 @@ import {console} from 'forge-std/Test.sol';
 import {IL1OpUSDCFactory} from 'interfaces/IL1OpUSDCFactory.sol';
 
 contract L1FactoryDeployAndSetup is Script {
+  address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
   address public deployer = vm.rememberKey(vm.envUint('MAINNET_PK'));
-  address public usdc = vm.envAddress('USDC');
 
   function run() public {
     vm.createSelectFork(vm.rpcUrl(vm.envString('MAINNET_RPC')));
     vm.startBroadcast(deployer);
     console.log('Deploying L1OpUSDCFactory ...');
-    IL1OpUSDCFactory _l1Factory = new L1OpUSDCFactory(usdc);
+    IL1OpUSDCFactory _l1Factory = new L1OpUSDCFactory(USDC);
     console.log('L1OpUSDCFactory deployed at:', address(_l1Factory));
     /// NOTE: Hardcode the address on `L1_FACTORY_MAINNET` inside the `.env` file
     vm.stopBroadcast();
